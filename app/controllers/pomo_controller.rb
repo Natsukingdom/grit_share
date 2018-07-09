d6df75090cbc8efbcdc7c125081d5a54a7296abe
NatsukiMaehara <natsuki.m.1992@gmail.com>
2018-07-09 14:03:58 +0900

    rspec追加 next to do → pomo model 追加

--- app/controllers/pomo_controller.rb
+++ app/controllers/pomo_controller.rb
@@ -1,2 +1,12 @@
 class PomoController < ApplicationController
+  def new
+    # @todo: 空の pomo を 渡すように実装する
+    @pomo = '未設定'
+  end
+
+  def list
+    # @todo: すべてのpomoを取得して渡すように実装する
+    @pomos = 'まだまだ'
+  end
+
 end
