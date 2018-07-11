fb94abd5dae31b433c03092c838f5eef185dad51
前原夏樹 <n_maehara@septeni-original.co.jp>
2018-07-11 13:30:07 +0900

    暫定的なコミット, 次は、ユーザの各属性のバリデーションを実装する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create test data' do
+    it 'create test data' do
+      test_user = FactoryBot.create(:user)
+      test_user.email
+      p test_user
+    end
+  end
 end
