0c48d693e7dbc6b32e525ddc7583d0e319ec9d42
NatsukiMaehara <natsuki.m.1992@gmail.com>
2018-07-11 09:04:22 +0900

    \#5 g scaffold user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :pomos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pomo', to: 'pomo#new'
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname "MyString"
+    mail "MyString"
+    password_digest "MyString"
+    birthday "2018-07-11"
+  end
+end
