b9425f251b8c3eb05503480c5958152020281482
前原夏樹 <n_maehara@septeni-original.co.jp>
2018-07-11 13:00:15 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root 'pomos#new'
   resources :users
   resources :pomos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pomo', to: 'pomo#new'
-  get 'pomo/list'
 end
