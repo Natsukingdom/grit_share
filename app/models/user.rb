6203d2d379e52851a52982968409ea56d30515eb
NatsukiMaehara <natsuki.m.1992@gmail.com>
2018-07-19 23:25:04 +0900

    fix #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 # Model of User
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   before_save { self.email.downcase! }
   validates :nickname, presence: true, length: { maximum: 100 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
