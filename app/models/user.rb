bba76f95fa0beaa887e550101b9b92e29a931a59
Natsukingdom <natsuki.m.1992@gmail.com>
2018-07-25 08:58:25 +0900

    fix test & able to register nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :nickname, presence: true, length: { maximum: 100 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 191 }, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true,
+                    length: { maximum: 191 },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :password, length: { minimum: 6 }, presence: true
   has_many :pomos
