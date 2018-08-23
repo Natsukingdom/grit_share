063b2ef2dcbf10ee9f1b216001d562a5a40775b0
前原夏樹 <n_maehara@septeni-original.co.jp>
2018-08-23 13:51:01 +0900

    [#3] 時間をデクリメントする機能を追加

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,4 +1,7 @@
+var timer;
+var counter = 60 * 25
 document.addEventListener('DOMContentLoaded', function() {
+        setSeconds(counter);
         document.getElementById('btn').addEventListener('click', function() {
             toggleBtnFunction();
         })
@@ -8,9 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function toggleBtnFunction() {
     var button = document.getElementById('btn');
     if(button.value == 'Start' || button.value == 'ReStart') {
+        startTimer();
         button.value = 'Stop';
         button.className = 'stop-timer';
-        startTimer();
     } else if(button.value == 'Stop') {
         stopTimer();
         button.value = 'ReStart';
@@ -18,10 +21,19 @@ function toggleBtnFunction() {
     }
 }
 
-function startTimer() {
 
+function startTimer() {
+    timer = setInterval(function() {
+        counter--;
+        setSeconds(counter);
+    }, 1000)
 }
 
 function stopTimer() {
+    clearInterval(timer);
+}
 
+function setSeconds(restSeconds) {
+    var seconds = document.getElementById('seconds')
+    seconds.textContent = restSeconds;
 }
