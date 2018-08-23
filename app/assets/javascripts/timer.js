44b03c4064aea075ca99ac17628f8c49b64efb8f
前原夏樹 <n_maehara@septeni-original.co.jp>
2018-08-23 11:54:43 +0900

    [#3] ボタンのトグル処理を実装

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,30 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-
         document.getElementById('btn').addEventListener('click', function() {
             toggleBtnFunction();
-        })
-
-        document.getElementsByClassName('start-timer').addEventListener('click', function() {
-            startTimer();
-        })
-
-        document.getElementsByClassName('stop-timer').addEventListener('click', function() {
-            stopTimer();
         })
     }
 )
 
 function toggleBtnFunction() {
     var button = document.getElementById('btn');
-    if(button.value == 'Start') {
+    if(button.value == 'Start' || button.value == 'ReStart') {
         button.value = 'Stop';
         button.className = 'stop-timer';
+        startTimer();
     } else if(button.value == 'Stop') {
-        button.value = 'Start';
+        stopTimer();
+        button.value = 'ReStart';
         button.className = 'start-timer';
     }
 }
 
 function startTimer() {
+
+}
+
+function stopTimer() {
 
 }
